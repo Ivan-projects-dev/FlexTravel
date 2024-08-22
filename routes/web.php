7f2f00c9262d539b4c6a9839b7c4be5c56f695f7
Ivan <ivandoska2006@gmail.com>
2024-08-22 22:54:45 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,20 +9,22 @@ use App\Http\Controllers\BookingController;
 Route::get('/', function () { return view('welcome'); });
 Route::get('/dashboard', function () { return view('dashboard'); })->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/public-reviews', [ReviewController::class, 'publicIndex'])->name('public.reviews');
+Route::resource('trips', TripController::class);
 Route::get('/trips', [TripController::class, 'index'])->name('trips.index');
 Route::get('/trips/{trip}', [TripController::class, 'show'])->name('trips.show');
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/trips/create', [TripController::class, 'create'])->name('trips.create'); 
+    Route::get('/trips/create', [TripController::class, 'create'])->name('trips.create');
     Route::post('/trips', [TripController::class, 'store'])->name('trips.store');
     Route::get('/trips/{trip}/edit', [TripController::class, 'edit'])->name('trips.edit');
     Route::patch('/trips/{trip}', [TripController::class, 'update'])->name('trips.update');
-    Route::delete('/trips/{trip}', [TripController::class, 'destroy'])->name('trips.destroy'); 
-    Route::post('/trips/{trip}/book', [BookingController::class, 'book'])->name('trips.book');
+    Route::delete('/trips/{trip}', [TripController::class, 'destroy'])->name('trips.destroy');
+    Route::get('bookings/{id}/edit', [BookingController::class, 'edit'])->name('bookings.edit');
 });
-Route::get('/bookings/create', [BookingController::class, 'create'])->name('bookings.create');
+Route::resource('bookings', BookingController::class);
+Route::post('/bookings', [BookingController::class, 'store'])->name('bookings.store');
 Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');
 Route::resource('/reviews', ReviewController::class)->names([
     'index' => 'reviews.index',
