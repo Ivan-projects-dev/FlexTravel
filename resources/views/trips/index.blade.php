e52033cf37d891114269c982352daf79e38f3f3d
Akknel <97848169+Akknel@users.noreply.github.com>
2024-08-25 22:24:11 +0200

    Update index.blade.php

--- resources/views/trips/index.blade.php
+++ resources/views/trips/index.blade.php
@@ -1,22 +1,28 @@
 @extends('trips.layout')
-
 @section('content')
 <div class="container">
     <div class="row" style="padding:20px;">
         <div class="col-12">
             <div class="card">
-                <div class="card-header">Trips</div>
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <span>Trips </span>
+                    <div class="input-group">
+                        <select id="sort" class="form-control" onchange="window.location.href=this.value">
+                            <option value="{{ route('trips.index', ['sort' => 'destination']) }}" {{ $sort == 'destination' ? 'selected' : '' }}>Destination</option>
+                            <option value="{{ route('trips.index', ['sort' => 'price']) }}" {{ $sort == 'price' ? 'selected' : '' }}>Price</option>
+                        </select>
+                    </div>
+                </div>
                 <div class="card-body">
                     @if(auth()->check() && auth()->user()->is_admin)
-                        <a href="{{ route('trips.create') }}" class="btn btn-success btn-sm" title="Add New Trip">
+                        <a href="{{ route('trips.create') }}" class="btn btn-success btn-sm mb-3" title="Add New Trip">
                             <i class="fa fa-plus" aria-hidden="true"></i> Add New
                         </a>
                     @endif
-                    <br/><br/>
                     <div class="row">
                         @foreach($trips as $trip)
-                            <div class="col-md-4">
-                                <div class="card mb-4">
+                            <div class="col-md-4 mb-4">
+                                <div class="card">
                                     <img class="card-img-top" src="{{ $trip->photo ? asset('storage/' . $trip->photo) : 'https://via.placeholder.com/150' }}" alt="Trip Photo">
                                     <div class="card-body">
                                         <h5 class="card-title">{{ $trip->destination }}</h5>
