8878d21c19f4181eb69f811ed87714b086168600
Akknel <97848169+Akknel@users.noreply.github.com>
2024-08-25 22:29:07 +0200

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,12 +4,28 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <a href="{{ route('trips.countries', ['country' => 'estonia']) }}" class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
+                        <div class="pt-3 sm:pt-5">
+                            <h2 class="text-xl font-semibold text-black dark:text-white">Estonia</h2>
+                            <p class="mt-4 text-sm/relaxed">Embark on a journey through Estonia with our bus trips, where you'll explore Tallinn's well-preserved medieval Old Town and its modern creative districts.</p>
+                        </div>
+                    </a>
+                    <a href="{{ route('trips.countries', ['country' => 'latvia']) }}" class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
+                        <div class="pt-3 sm:pt-5">
+                            <h2 class="text-xl font-semibold text-black dark:text-white">Latvia</h2>
+                            <p class="mt-4 text-sm/relaxed">Discover the charm of Latvia with our bus trips, exploring the historic streets of Riga, the capital known for its stunning Art Nouveau architecture and vibrant cultural scene.</p>
+                        </div>
+                    </a>
+                    <a href="{{ route('trips.countries', ['country' => 'lithuania']) }}" class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
+                        <div class="pt-3 sm:pt-5">
+                            <h2 class="text-xl font-semibold text-black dark:text-white">Lithuania</h2>
+                            <p class="mt-4 text-sm/relaxed">Experience Lithuania's rich history and culture on our bus tours, with visits to the medieval Old Town of Vilnius, a UNESCO World Heritage site.</p>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
